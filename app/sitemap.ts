127eae6480ddddac7fb0d4c0b39c45d0a35c5371
deangudgeon123-hub <dean.gudgeon123@gmail.com>
2026-08-18 17:14:27 +0100

    Add XML sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://sussexsiteco.co.uk',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ];
+}
